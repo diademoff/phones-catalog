bf45dfa58444fc084f817365a4633d6c9ce6d3c7
diademoff <diademoff@yandex.ru>
2021-06-11 16:28:26 +0300

    add phone categories

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 var optionsAndroid = new Map([
-    ['Samsung', ["Galaxy Note", "Galaxy Fold"]],
-    ['OPPO', ["Find X", "Find X2", "Find X3", "A Series A1", "A Series A3", "A Series A5", "A Series A7", "A Series A9", "F Series F1"]],
-    ['Realme', ["Realme X", "Realme G", "Realme Q", "Realme V", "Realme C"]]
+    ["Oppo", ["Find", "Reno", "Neo", "A Series", "F Series", "K Series"]],
+    ["Realme", ["X Series", "G Series", "Q Series", "V Series", "C Series", "Narzo", "8 Series", "7 Series", "6 Series", "5 Series", "3 Series"]],
+    ["Vivo", ["X Series", "V Series", "Y Series", "NEX", "iQOO"]],
+    ["OnePlus", ["Nord", "9 Series", "8 Series", "7 Series", "6 Series"]],
+    ["Samsung", ["Galaxy Note", "Galaxy Fold", "Galaxy Z Series", "Galaxy S Series", "Galaxy A Series", "Galaxy M Series", "Galaxy F Series"]],
+    ["Xiaomi", ["Note", "Mix", "Max", "A Series", "MI 11", "MI 10", "MI 9", "MI 8", "Redmi Note", "Redmi 9", "Redmi 8", "Redmi 7", "Redmi 6", "Redmi 5", "K40", "K30", "K20", "Poco F", "Poco X", "Poco M", "Poco C", "Black Shark", "Black Shark 2", "Black Shark 3", "Black Shark 4"]],
+    ["Motorola", ["Edge", "Edge S", "Edge+", "Moto E", "Moto G", "Moto Z"]],
+    ["Asus", ["Zenfone 7", "Zenfone 6", "Zenfone 5", "ROG Phone 5", "ROG Phone 3", "ROG Phone 2", "ROG Phone"]],
+    ["ZTE", ["Red Magic", "Z Series", "Axon", "Blade", "S30 Pro", "S30", "S30 SE"]],
+    ["Nokia", ["X20", "X10", "X71", "1 Series", "2 Series", "3 Series", "4 Series", "5 Series", "6 Series", "7 Series", "8 Series", "9 Series", "G Series", "C Series"]],
+    ["Sony", ["Xperia 1 Series", "Xperia 5 Series", "Xperia 10 Series", "Xperia L Series", "Xperia Pro"]],
+    ["Meizu", ["18 Series", "17 Series", "16 Series", "15 Series", "M Series", "V8 Series", "X8 Series"]],
+    ["Honor", ["30 Series", "20 Series", "10 Series", "9 Series", "V Series", "View", "PLay"]],
+    ["Huawei", ["P40", "P30", "P20", "P Smart", "Nova Series", "Y Series", "Mate Series", "Enjoy Series"]],
+    ["Google", ["Pixel 5", "Pixel 4", "Pixel 3"]]
 ]);
 
 var optionsIOS = new Map([
-    ['IPhone', []],
+    ["IPhone", ["12 Series", "11 Series", "X Series", "8 Series", "7 Series", "6 Series", "5 Series"]]
 ]);
 
 
@@ -20,7 +32,12 @@ function fillOptionsBySub(sub_selected) {
     if (val) {
         fillOptionsUsingArray(val);
     } else {
-        console.log(sub_selected + " не найден");
+        val = optionsIOS.get(sub_selected);
+        if (val) {
+            fillOptionsUsingArray(val);
+        } else {
+            console.log(sub_selected + " не найден");
+        }
     }
 }
 
